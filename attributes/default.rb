923df65dbcd73438acfb7f63227bdc57e17d11af
Luke Thorne <luke.thorne@kbslp.com>
2017-10-24 15:07:02 -0700

    switch back to sha256 chcksum

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['windows_logrotate']['version'] = '0.0.0.18_20170228'
-default['windows_logrotate']['md5'] = '97cf9e2f242a95b026d8d473bc93bf73'
+default['windows_logrotate']['sha256'] = '186bf4470b97a649997457992c2f80192a923503edaf144367391d3054c2ab73'
 default['windows_logrotate']['install_dir'] = "#{ENV['SYSTEMDRIVE']}\\logrotate"
 default['windows_logrotate']['zip_filename'] = "logrotateSetup_#{node['windows_logrotate']['version']}.zip"
 default['windows_logrotate']['url'] = 'https://pilotfiber.dl.sourceforge.net/project' \
