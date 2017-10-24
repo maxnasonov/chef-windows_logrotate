348522f246cf5788834ad325c43bb0e67d287a2e
Luke Thorne <the.thorne48@gmail.com>
2017-10-24 14:52:12 -0700

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,6 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 
 CACHE = Chef::Config[:file_cache_path]
-VERSION = '0.0.0.17_20170116'.freeze
+VERSION = '0.0.0.18_20170228'.freeze
 
 ChefSpec::Coverage.start!
