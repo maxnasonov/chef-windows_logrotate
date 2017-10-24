c12e07b7d747f80bdf6cddd14a9684357015684f
Luke Thorne <the.thorne48@gmail.com>
2017-10-24 13:15:06 -0700

    Update default.rb
    move to the new version so the cert is up to date

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default['windows_logrotate']['version'] = '0.0.0.17_20170116'
-default['windows_logrotate']['sha256'] = 'd82efafdf085ef98a72dade1c7ac66c303f8fa902a7a80a5dceba3062e0f07fb'
+default['windows_logrotate']['version'] = '0.0.0.18_20170228'
+default['windows_logrotate']['md5'] = '97cf9e2f242a95b026d8d473bc93bf73'
 default['windows_logrotate']['install_dir'] = "#{ENV['SYSTEMDRIVE']}\\logrotate"
 default['windows_logrotate']['zip_filename'] = "logrotateSetup_#{node['windows_logrotate']['version']}.zip"
 default['windows_logrotate']['url'] = 'https://pilotfiber.dl.sourceforge.net/project' \
